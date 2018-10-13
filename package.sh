a10d7461f2777776cd8f8c10451c855b6d76aab1
ayan4m1 <andrew@bulletlogic.com>
2018-10-12 23:54:27 -0400

    add assembly version information into nupkg during package.sh

--- package.sh
+++ package.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/bash
 
-if [ -z $TRAVIS_BUILD_NUMBER ]; then
+if [ -z "$TRAVIS_BUILD_NUMBER" ]; then
 	VERSION=1
 else
 	VERSION=$TRAVIS_BUILD_NUMBER
 fi
 
-sed -i "s^\(Assembly\(Informational\|File\)Version(\d34[0-9]\+\.[0-9]\+\.[0-9]\+\.\)[0-9]\+^\1$VERSION^" "Asphalt-ModKit/Properties/AssemblyInfo.cs"
+if [ ! -z "$TRAVIS_BRANCH" ]; then
+	sed -i "s#\(AssemblyConfiguration(\d34\).*\d34#\1$TRAVIS_BRANCH\d34#" "Asphalt-ModKit/Properties/AssemblyInfo.cs"
+fi
+
+sed -i "s#\(Assembly\(Informational\|File\)\?Version(\d34[0-9]\+\.[0-9]\+\.[0-9]\+\.\)[0-9]\+#\1$VERSION#" "Asphalt-ModKit/Properties/AssemblyInfo.cs"
+
+ASSEMBLY_VERSION=$(cat Asphalt-ModKit/Properties/AssemblyInfo.cs | grep -oP 'AssemblyVersion\("\K[0-9]+.[0-9]+.[0-9]+.[0-9]+')
+OUTPUT_FILE="AsphaltModKit.$ASSEMBLY_VERSION.nupkg"
+
+nuget pack "Asphalt-ModKit/Asphalt.nuspec" -Version $ASSEMBLY_VERSION
