87c7c629b9b77dd34edf036878df830189a51ce2
ayan4m1 <andrew@bulletlogic.com>
2018-10-12 23:52:20 -0400

    refactor project/solution names, update and overhaul .nuspec, clean up AssemblyInfo for both projects

--- Asphalt-ModKit/Properties/AssemblyInfo.cs
+++ Asphalt-ModKit/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Asphalt-ModKit")]
+[assembly: AssemblyTitle("AsphaltModKit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Asphalt-ModKit")]
-[assembly: AssemblyCopyright("Copyright © Eco Mod Developers 2018")]
+[assembly: AssemblyCompany("Eco Mods Developers")]
+[assembly: AssemblyProduct("AsphaltModKit")]
+[assembly: AssemblyCopyright("Copyright © Eco Mods Developers 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("bbea3dfa-52e0-448d-832f-c7bf96e3f3e8")]
-[assembly: AssemblyVersion("0.5.1.0")]
-[assembly: AssemblyFileVersion("0.5.1.0")]
+[assembly: AssemblyVersion("0.5.1.1")]
+[assembly: AssemblyFileVersion("0.5.1.1")]
