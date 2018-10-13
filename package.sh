b3686d9987db04acd8c86e3b6f475a81a4958084
ayan4m1 <andrew@bulletlogic.com>
2018-10-13 00:18:16 -0400

    correct mistakes in file paths

--- package.sh
+++ package.sh
@@ -7,12 +7,12 @@ else
 fi
 
 if [ ! -z "$TRAVIS_BRANCH" ]; then
-	sed -i "s#\(AssemblyConfiguration(\d34\).*\d34#\1$TRAVIS_BRANCH\d34#" "Asphalt-ModKit/Properties/AssemblyInfo.cs"
+	sed -i "s#\(AssemblyConfiguration(\d34\).*\d34#\1$TRAVIS_BRANCH\d34#" "Asphalt/Properties/AssemblyInfo.cs"
 fi
 
-sed -i "s#\(Assembly\(Informational\|File\)\?Version(\d34[0-9]\+\.[0-9]\+\.[0-9]\+\.\)[0-9]\+#\1$VERSION#" "Asphalt-ModKit/Properties/AssemblyInfo.cs"
+sed -i "s#\(Assembly\(Informational\|File\)\?Version(\d34[0-9]\+\.[0-9]\+\.[0-9]\+\.\)[0-9]\+#\1$VERSION#" "AsphaltProperties/AssemblyInfo.cs"
 
-ASSEMBLY_VERSION=$(cat Asphalt-ModKit/Properties/AssemblyInfo.cs | grep -oP 'AssemblyVersion\("\K[0-9]+.[0-9]+.[0-9]+.[0-9]+')
+ASSEMBLY_VERSION=$(cat Asphalt/Properties/AssemblyInfo.cs | grep -oP 'AssemblyVersion\("\K[0-9]+.[0-9]+.[0-9]+.[0-9]+')
 OUTPUT_FILE="AsphaltModKit.$ASSEMBLY_VERSION.nupkg"
 
-nuget pack "Asphalt-ModKit/Asphalt.nuspec" -Version $ASSEMBLY_VERSION
+nuget pack "Asphalt/Asphalt.nuspec" -Version $ASSEMBLY_VERSION
