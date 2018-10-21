5f6c05e95719fba62397242556092089aecebf50
ayan4m1 <andrew@bulletlogic.com>
2018-10-21 18:29:16 -0400

    rename AsphaltModKit to Asphalt

--- Asphalt/Properties/AssemblyInfo.cs
+++ Asphalt/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("AsphaltModKit")]
+[assembly: AssemblyTitle("Asphalt")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Eco Mods Developers")]
-[assembly: AssemblyProduct("AsphaltModKit")]
+[assembly: AssemblyProduct("Asphalt")]
 [assembly: AssemblyCopyright("Copyright © Eco Mods Developers 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("bbea3dfa-52e0-448d-832f-c7bf96e3f3e8")]
 [assembly: AssemblyVersion("0.5.1.1")]
 [assembly: AssemblyFileVersion("0.5.1.1")]
-[assembly: InternalsVisibleTo("AsphaltTests")]
+[assembly: InternalsVisibleTo("Asphalt.Tests")]
